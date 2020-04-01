2c2968d64236e69d3f2b3dd892cda62667570156
Pratyush <pratyush.singh@mindtree.com>
2020-04-02 01:01:04 +0530

    Updated environment files for azure endpoint

--- BagTrackClientApp/src/environments/environment.prod.ts
+++ BagTrackClientApp/src/environments/environment.prod.ts
@@ -1,9 +1,15 @@
 export const environment = {
   production: true,
 
-  bagtrackServiceEndpoint: 'http://localhost:8080',
-  mintoServiceEndpoint: 'http://localhost:8081',
-  flightDelayEndpoint: 'http://localhost:8082',
-  //triggerPredictionEndpoint: 'http://localhost:3300'
-  triggerPredictionEndpoint: 'https://angular.io/'
+  //bagtrackServiceEndpoint: 'http://localhost:8080',
+  //mintoServiceEndpoint: 'http://localhost:8081',
+  //flightDelayEndpoint: 'http://localhost:8082',
+  //triggerPredictionEndpoint: 'http://localhost:3300',
+  //triggerPredictionEndpoint: 'https://angular.io/'
+
+  bagtrackServiceEndpoint: 'https://airlinemindtree.azurewebsites.net/airline-app',
+  mintoServiceEndpoint: 'https://travelswift.azurewebsites.net/minto-app',
+  flightDelayEndpoint: 'https://airlinemindtree.azurewebsites.net/flightdelay',
+  triggerPredictionEndpoint: 'http://swiftvm.centralus.cloudapp.azure.com:3000',
+  //triggerPredictionEndpoint: 'https://angular.io/'
 };
--- BagTrackClientApp/src/environments/environment.ts
+++ BagTrackClientApp/src/environments/environment.ts
@@ -5,11 +5,17 @@
 export const environment = {
   production: false,
 
-  bagtrackServiceEndpoint: 'http://localhost:8080',
-  mintoServiceEndpoint: 'http://localhost:8081',
-  flightDelayEndpoint: 'http://localhost:8082',
+  //bagtrackServiceEndpoint: 'http://localhost:8080',
+  //mintoServiceEndpoint: 'http://localhost:8081',
+  //flightDelayEndpoint: 'http://localhost:8082',
   //triggerPredictionEndpoint: 'http://localhost:3300'
-  triggerPredictionEndpoint: 'https://angular.io/'
+  //triggerPredictionEndpoint: 'https://angular.io/'
+
+  bagtrackServiceEndpoint: 'https://airlinemindtree.azurewebsites.net/airline-app',
+  mintoServiceEndpoint: 'https://travelswift.azurewebsites.net/minto-app',
+  flightDelayEndpoint: 'https://airlinemindtree.azurewebsites.net/flightdelay',
+  triggerPredictionEndpoint: 'http://swiftvm.centralus.cloudapp.azure.com:3000',
+  //triggerPredictionEndpoint: 'https://angular.io/'
 };
 
 /*
