589ba722bd7ffbb74d66d85ca34333c0fbc2d826
pritamghosh <19pritamghosh93@gmail.com>
2020-04-02 12:47:51 +0100

    servlet initializer

--- bag-tracker-master/src/main/java/com/bangalorewest/bagtracker/BagTrackerApplication.java
+++ bag-tracker-master/src/main/java/com/bangalorewest/bagtracker/BagTrackerApplication.java
@@ -2,10 +2,13 @@ package com.bangalorewest.bagtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import com.bangalorewest.bagtracker.util.ConfigReader;
 
@@ -14,11 +17,17 @@ import com.bangalorewest.bagtracker.util.ConfigReader;
  *
  */
 @SpringBootApplication
+@EnableSwagger2
 @EnableConfigurationProperties(ConfigReader.class)
-public class BagTrackerApplication {
+public class BagTrackerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BagTrackerApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BagTrackerApplication.class);
 	}
 	
     @Bean
